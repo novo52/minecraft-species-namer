24c9121391ae80f17006bf9b02f0edd8e359ab14
Tyler <littleboboonska@gmail.com>
2019-08-31 14:55:18 -0600

    update namer.js

--- namer.js
+++ namer.js
@@ -1,20 +1,22 @@
 const nameLists = require('./nameLists.js');
 
 let args = process.argv.slice(2);
-console.log(createSpeciesName([args]));
+console.log(createSpeciesName(args));
 
-function createSpeciesName(names) {
-  for (i in names) {
-    let name = names[i];
+function createSpeciesName(name) {
+  if (typeof name == 'string') {
+    name = name.split(' ');
+  }
 
-    let starter = nameLists.starters[name[0]];
-    let ending = nameLists.endings[name[1]];
-    let output = '' + starter + ending;
-    if (typeof starter != 'undefined' && typeof ending != 'undefined') {
-      return output;
-    } else {
-      return false;
-    }
+  if (typeof name != 'object' || name.length > 2) return TypeError('Invalid args');
+
+  let starter = nameLists.starters[name[0]];
+  let ending = nameLists.endings[name[1]];
+  let output = '' + starter + ending;
+  if (typeof starter != 'undefined' && typeof ending != 'undefined') {
+    return output;
+  } else {
+    return Error('No translation found');
   }
 }
 
