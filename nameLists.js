8449699ce80377034fe0ba855e55fe7a98663d2d
Tyler <littleboboonska@gmail.com>
2019-08-31 14:35:21 -0600

    update nameList.js

--- nameLists.js
+++ nameLists.js
@@ -1,20 +1,32 @@
-const starters = {
+const starters = { // If adding to this list, make sure the translation is somewhat accurate (I use google translate) and that it ends with a vowel (if it doesn't, add one!)
 slow: 'tarda',
-big: 'magnum',
+big: 'magnu',
 fast: 'rapidi',
 stone: 'lapi',
 grass: 'herba',
 dirt: 'lutu',
-item: 'res',
+item: 'resi',
+biome: 'biome',
+nether: 'inferno',
+tool: 'vasi',
+pickaxe: 'dolabri',
+food: 'cibu',
+
 };
 
-const endings = {
+const endings = { // If adding to this list, make sure that it ends with 'us', 'or', 'ii', or some variation on the theme.
 miner: 'metallicus',
 crafter: 'formatus',
 holder: 'possessor',
 breaker: 'ruptor',
 dropper: 'dejectus',
 hoarder: 'recondor',
+eater: 'comedentotii',
+finder: 'repertor',
+thief: 'fur',
+runner: 'cursor',
+fool: 'fatuus',
+
 };
 
 module.exports = {
