765ad20626e38d069b90f42138bb6b5ad8d5a68f
Tyler <littleboboonska@gmail.com>
2019-08-31 13:29:12 -0600

    Update namer.js

--- namer.js
+++ namer.js
@@ -1,13 +1,19 @@
 const objectsAndAdjectives = {slow: 'tarda', big: 'magnum', fast: 'rapidi', stone: 'lapi', grass: 'herba', dirt: 'lutu', item: 'res'}
 const subjects = {miner: 'metallicus', crafter: 'formatus', holder: 'possessor', breaker: 'ruptor', dropper: 'dejectus', }
 
-let names = [['slow', 'miner'], ['dirt', 'holder'], ['item', 'dropper']]
+let args = process.argv.slice(2);
+console.log(createSpeciesName([args]))
 
-for (i in names) {
-  let name = names[i];
-  let output = name[0] + ' ' + name[1] + ': Minecrafticus ';
-  let starter = objectsAndAdjectives[name[0]];
-  let ending = subjects[name[1]];
-  output += '' + starter + ending;
-  console.log(output);
+function createSpeciesName(names) {
+  for (i in names) {
+    let name = names[i];
+    let starter = objectsAndAdjectives[name[0]];
+    let ending = subjects[name[1]];
+    let output = '' + starter + ending;
+    if (typeof starter != 'undefined' && typeof ending != 'undefined') {
+      return output;
+    } else {
+      return false;
+    }
+  }
 }
